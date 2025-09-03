fc8ae3e339b364bf360605b808ffe3c4da046ad8
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-09-03 13:40:36 +0100

    Refactors student REST controller
    Replaces direct repository access with service interface implementation.
    This change promotes loose coupling and improves testability by
    introducing an abstraction layer for student data access.
    
    Routes requests to the service layer for data retrieval, updates,
    and creation, enabling more flexible and maintainable code.

--- src/main/java/com/learn/demostudentpay/controllers/StudentRestController.java
+++ src/main/java/com/learn/demostudentpay/controllers/StudentRestController.java
@@ -1,72 +1,46 @@
 package com.learn.demostudentpay.controllers;
 
 import com.learn.demostudentpay.entites.Student;
-import com.learn.demostudentpay.repositorys.StudentRepository;
+import com.learn.demostudentpay.services.serviceInterface.StudentServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/student")
 public class StudentRestController {
-    private final StudentRepository studentRepository;
+
+    private final StudentServiceInterface studentService;
 
     @Autowired
-    public StudentRestController(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
+    public StudentRestController(StudentServiceInterface studentServiceImpl) {
+        this.studentService = studentServiceImpl;
     }
 
     @GetMapping("/all")
     public List<Student> getAllStudents() {
-        return studentRepository.findAll();
+        return studentService.getAllStudentsImpl();
     }
 
     @GetMapping("/{code}")
     public Student getStudentByCode(@PathVariable String code) {
-        return studentRepository.findByCode(code);
+        return studentService.getStudentByCodeImpl(code);
     }
 
     @GetMapping("/programId")
     public List<Student> getStudentByProgramId(@RequestParam String programId) {
-        return studentRepository.findByProgramID(programId);
+        return studentService.getStudentByProgramIdImpl(programId);
     }
 
     @GetMapping(path = "/photo/{code}")
     public ResponseEntity<Resource> getImageByCode(@PathVariable String code) throws IOException {
-        Student student = studentRepository.findByCode(code);
-
-        Path filePath = Paths.get(System.getProperty("user.home"), "edd-demo-yousfiData", "users-photos");
-        if (!Files.exists(filePath)) Files.createDirectory(filePath);
-        //System.out.println(filePath.toString());
-
-        Path PathFinal = Paths.get(filePath.resolve(student.getPhoto()).toUri());
-        //System.out.println(PathFinal+" photoID "+student.getPhoto());
-
-        if (!Files.exists(PathFinal)) {
-            throw new FileNotFoundException("Image not found  "+ PathFinal);
-        }
-        Resource resource = new UrlResource(PathFinal.toUri());
-
-        if (!resource.exists()) {
-            throw new FileNotFoundException("Image not found  "+ PathFinal);
-        }
-
-        String contentType = Files.probeContentType(PathFinal);
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .body(resource);
+        return studentService.getImageByCodeImpl(code);
     }
 
     /*
@@ -74,44 +48,16 @@ public class StudentRestController {
      * */
     @PutMapping("/programId/update/{code}")
     public Student updateProgramByCode(@RequestParam String programId, @PathVariable String code) {
-        Student student = studentRepository.findByCode(code);
-        student.setProgramID(programId);
-        return studentRepository.save(student);
+        return studentService.updateProgramByCodeImpl(programId, code);
     }
 
     /*
-     * Post Mothed
+     * Post Methods
      * */
     @PostMapping(path = "/newStudent/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Student createStudent(@RequestParam MultipartFile file, String firstName,
                                  String lastName, String programID) throws IOException {
-
-        Path filePath = Paths.get(System.getProperty("user.home"), "edd-demo-yousfiData", "users-photos");
-        if (!Files.exists(filePath)) Files.createDirectory(filePath);
-
-        String originalFilename = file.getOriginalFilename();
-        String extension = "";
-
-        if (originalFilename != null && originalFilename.contains(".")) {
-            extension = originalFilename.substring(originalFilename.lastIndexOf("."));
-        }
-
-        //String fileName = studentRepository.findByCode(programID).getCode() +  extension;
-        String fileName = UUID.randomUUID() + extension;
-        Path destination = filePath.resolve(fileName);
-        file.transferTo(destination);
-
-        Student student = Student.builder()
-                .Id(UUID.randomUUID().toString())
-                .firstName(firstName)
-                .lastName(lastName)
-                .code("STU" + UUID.randomUUID())
-                .programID(programID)
-                .Photo(fileName)
-                .build();
-
-
-        return studentRepository.save(student);
+        return studentService.createStudentImpl(file, firstName, lastName, programID);
     }
 
 }
