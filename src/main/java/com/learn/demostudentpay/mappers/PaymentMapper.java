ff44fd122e16fdb3f44b46b3dbba5c8f5a013f1d
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-09-18 14:47:06 +0100

    Refactors PaymentMapper for DTO conversions
    Improves PaymentMapper by adding explicit mappings for converting between Payment entities and DTOs (Request and Response).
    This change enhances clarity and maintainability by defining clear mapping rules.
    It also addresses the mapping of the student's full name from the entity to the response DTO.

--- src/main/java/com/learn/demostudentpay/mappers/PaymentMapper.java
+++ src/main/java/com/learn/demostudentpay/mappers/PaymentMapper.java
@@ -12,20 +12,34 @@ import java.util.List;
 @Mapper(componentModel = "spring", uses = StudentMapper.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PaymentMapper {
 
+    // Entity -> RequestDTO
     @Mapping(source = "date", target = "dateOfPayment")
     @Mapping(source = "amount", target = "amountOfPayment")
     @Mapping(source = "type", target = "paymentType")
     @Mapping(source = "status", target = "paymentStatus")
     @Mapping(source = "file", target = "paymentFilePdf")
-    @Mapping(source = "student", target = "studentId")
+    @Mapping(source = "student.id", target = "studentId")
     PaymentRequestDTO toPaymentRequestDTO(Payment payment);
 
+    // RequestDTO -> Entity
+    @Mapping(source = "dateOfPayment", target = "date")
+    @Mapping(source = "amountOfPayment", target = "amount")
+    @Mapping(source = "paymentType", target = "type")
+    @Mapping(source = "paymentStatus", target = "status")
+    @Mapping(source = "paymentFilePdf", target = "file")
+    @Mapping(target = "student", ignore = true) // On ignore student pour le moment
+    @Mapping(target = "id", ignore = true) // On ignore l'ID, car il est généré
     Payment toEntity(PaymentRequestDTO paymentRequestDTO);
 
-    List<PaymentRequestDTO> toPaymentRequestDTOList(List<Payment> payments);
-
-    @Mapping(source = "student.fullName", target = "studentFullName")
+    // Entity -> ResponseDTO
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "date", target = "date")
+    @Mapping(source = "amount", target = "amount")
+    @Mapping(source = "type", target = "type")
+    @Mapping(source = "status", target = "status")
+    @Mapping(source = "file", target = "file")
+    @Mapping(expression = "java(entity.getStudent() != null ? entity.getStudent().getFullName() : null)", target = "studentFullName")
     PaymentResponseDTO toResponseDto(Payment entity);
 
-    List<PaymentResponseDTO> toResponseDtoList(List<Payment> payment);
+    List<PaymentResponseDTO> toResponseDtoList(List<Payment> payments);
 }
